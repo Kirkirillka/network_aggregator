92d34251cce9c7655f68c470849fc53ef8339d2b
kirill <kirillkukarcev@gmail.com>
2018-08-29 23:50:58 +0300

    Add comments to constants for Scan class.

--- consts.py
+++ consts.py
@@ -1,11 +1,9 @@
-SYNC_SCAN = 1
-ASYNCSCAN = 2
-
-
+# Constants to specify what mode to use in Scanner class
 TCP_SCAN = '-sT'
 UDP_SCAN = '-sU'
 SERVICE_VERSION_SCAN = '-sV'
 
-
+# Constants to constraint range of ports to scan.
+# According to RFC 6335 maximum port is limited to 2 **16 -1
 DEFAULT_PORT_RANGE = range(1,100)
 MAX_RANGE = range(1, 2**16)
