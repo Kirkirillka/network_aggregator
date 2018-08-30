4c13bc03336be19bd0172a871b583c71b2b03639
kirill <kirillkukarcev@gmail.com>
2018-08-31 01:24:56 +0300

    validate_net_data logic split to process ADDRESS and NETWORK more separately and precisely.

--- utils.py
+++ utils.py
@@ -1,33 +1,33 @@
 import re
 import ipaddress
+from consts import ADDRESS,NETWORK
 
 
 def validate_net_data(net_data):
     """
-    # A function to check if supplied net_data meets model's requirement. Thus net_data must be a dict with
-    specified fields
+        A function to check if supplied net_data meets model's requirement. Thus net_data must be a dict with
+        specified fields
 
-    Examples of net_data:
-        net_data1 = {
-                    "value": "192.168.34.4",
-                    "type": "host"
-                    }
+        Examples:
+            net_data1 = {
+                        "value": "192.168.34.4",
+                        "type": "host"
+                        }
 
-        net_data2 = {
-                    "value": "192.168.34.0/24",
-                    "type": "network"
-                    }
+            net_data2 = {
+                        "value": "192.168.34.0/24",
+                        "type": "network"
+                        }
 
-        net_data3 = {
-                    "value": "192.168.34.4",
-                    "type": "host",
-                    "os": "Linux",
-                    "ports": [21, 22, 80 ,443]
-                    }
+            net_data3 = {
+                        "value": "192.168.34.4",
+                        "type": "host",
+                        "os": "Linux",
+                        "ports": [21, 22, 80 ,443]
+                        }
 
-
-    :param net_data: a dict with specified fields .
-    :return: True if net_data has required fields and they are in valid formats otherwise False.
+    :param net_data: dict: A dict with specified fields .
+    :return: bool: True if net_data has required fields and they are in valid formats otherwise False.
     """
 
     # These fields must be in supplied net_data
@@ -35,26 +35,43 @@ def validate_net_data(net_data):
     # Extra fields are needed to just help remember what fields can be there in applied data
     extra_fields = ['os', 'ports', 'users', 'supernet']
 
-    # Supplied data must be in dictionary form
+    # Supplied data must be in dictionary form.
     if not isinstance(net_data, dict):
         return False
 
-    # Check whether supplied dictionary has all required fields
-    if not all(field in net_data for field in required_fields):
-        return False
+    # Check whether supplied dictionary has all required fields.
+    if all(field in net_data for field in required_fields):
 
-    return True
+        net_type = net_data.get('type')
+        net_value = net_data.get('value')
+
+        # Check if network
+        if net_type is NETWORK:
+            if not is_network(net_value):
+                return False
+
+            return True
+
+        # Check if address
+        if net_type is ADDRESS:
+            if not is_addr(net_value):
+                return False
+            return True
+
+    # Check failed
+    return False
 
 
 def is_addr(addr):
     """
         Checks if supplied string is valid host address. Can be both 192.168.0.23 or 192.168.0.23/32 (CIDR format).
         Only IPv4 right now.
-    :param addr: a string represented a host address.
-    :return: True if a string is valid host address otherwise false.
+
+    :param addr: str: a string represented a host address.
+    :return: bool: True if a string is valid host address otherwise false.
     """
     # It should be better to insert several patterns for both IPv4/IPv6.
-    # Although, I haven't enough time I have put only pattern for x.x.x.x/24 IPv4 networks
+    # Although, I have not enough time so I have put only pattern for x.x.x.x/24 IPv4 networks
     patterns = [r'^(\d{1,3}).(\d{1,3}).(\d{1,3}).(\d{1,3})(\/32)?$']
     if any(re.match(pattern, addr) for pattern in patterns):
         return True
@@ -64,8 +81,9 @@ def is_addr(addr):
 def is_network(net):
     """
         Checks if supplied string is valid CIDR network address (only IPv4).
-    :param net:  a string to validate CIDR format.
-    :return: True if a given string is a valid CIDR network address otherwise False.
+
+    :param net: str:  a string to validate CIDR format.
+    :return: bool: True if a given string is a valid CIDR network address otherwise False.
     """
     # It should be better to insert several patterns for both IPv4/IPv6.
     # Although, I haven't enough time I have put only pattern for x.x.x.x/24 IPv4 networks
@@ -77,9 +95,14 @@ def is_network(net):
 
 def is_supernet(net, supernet):
     """
-        Checks for given net that it's overlapped by supplied supernet,
-            e.g. 192.168.13.0/25 is subnet of 192.168.13.0/24.
-        Both net and supernet must be given in CIDR format (IPv4 only).
+        Checks for given net that it's overlapped by supplied supernet.
+
+        Notes:
+            Both net and supernet must be given in CIDR format (IPv4 only).
+
+        Examples:
+            192.168.13.0/25 is subnet of 192.168.13.0/24.
+
     :param net: a string represented network address in CIDR format to validate it's overlapped by supernet.
     :param supernet: a string represented network address in CIDR format which mask prefix is less than a given one.
     :return: True if net is overlapped by supernet, e.g net is a subnet of supernet.
